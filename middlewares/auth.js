212c615922c5cc3615901bf535a65e67caa93087
snkmqn <160516534+snkmqn@users.noreply.github.com>
2025-03-13 16:18:59 +0500

    updated

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,21 +1,21 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-    const token = req.header("Authorization") && req.header("Authorization").replace('Bearer ', '');
+    const token = req.header("Authorization")?.replace("Bearer ", "");
 
     if (!token) {
-        return res.status(401).json({message: "Access Denied"});
+        return res.status(401).json({message: "Access denied"});
     }
 
     try {
-        console.log('Received token:', token);
+        console.log('Received JWT:', token);
         const decoded = jwt.verify(token, process.env.JWT_SECRET, {algorithms: ['HS256']});
-        console.log('Decoded User:', decoded);
+        console.log('Decoded JWT:', decoded);
         req.user = {userId : decoded.userId};
         next()
-    } catch (err) {
-        console.error("Invalid Token:", err);
-        res.status(400).json({message: "Invalid Token"});
+    } catch (error) {
+        console.error("Invalid JWT:", error);
+        res.status(400).json({message: "Invalid JWT"});
     }
 };
 
