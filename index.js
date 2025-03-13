fb3598cff3c31baf10c6118bf02ed0a9ae903387
snkmqn <160516534+snkmqn@users.noreply.github.com>
2025-03-13 16:23:49 +0500

    updated

--- index.js
+++ index.js
@@ -12,60 +12,71 @@ const userRoutes = require("./routes/getUser");
 const adsRoutes = require("./routes/ads");
 const profileRoutes = require("./routes/profile");
 const checkEmailProfile = require("./routes/checkEmailProfile");
-const checkUsernameProfile = require("./routes/checkUsernameProfile")
-const reviewRoutes = require("./routes/reviews")
+const checkUsernameProfile = require("./routes/checkUsernameProfile");
+const reviewRoutes = require("./routes/reviews");
 
 const app = express();
 const PORT = process.env.PORT;
 
-connectDB();
+const startServer = async () => {
+    try {
+        await connectDB();
 
-app.use(express.json({limit: "10mb"}));
-app.use(express.urlencoded({extended: true, limit: "10mb"}));
+        app.use(express.json({limit: "10mb"}));
+        app.use(express.urlencoded({extended: true, limit: "10mb"}));
 
-app.use((err, req, res, next) => {
-    if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
-        return res.status(400).json({error: "Invalid JSON format"});
+        app.use(express.static(path.join(__dirname, "templates")));
+        app.use(express.static(path.join(__dirname, "static")));
+        app.use("/images", express.static(path.join(__dirname, "images")));
+
+
+        const serveHTML = (req, res, filePath) => {
+            res.sendFile(path.join(__dirname, "templates", filePath));
+        };
+
+        app.get("/", (req, res) => serveHTML(req, res, "index.html"));
+        app.get("/signup", (req, res) => serveHTML(req, res, "authentication/signup.html"));
+        app.get("/login", (req, res) => serveHTML(req, res, "authentication/login.html"));
+        app.get("/myAds", (req, res) => serveHTML(req, res, "carTrading/myAds.html"));
+        app.get("/profile", (req, res) => serveHTML(req, res, "carTrading/profile.html"));
+        app.get("/reviewExample", (req, res) => serveHTML(req, res, "carTrading/reviewExample.html"));
+        app.get("/index", (req, res) => serveHTML(req, res, "index.html"));
+
+
+        app.use("/api/auth", signUpRoutes);
+        app.use("/api/auth", loginRoutes);
+        app.use("/api/check", checkUsername);
+        app.use("/api/check", checkEmail);
+        app.use("/api", searchRoutes);
+        app.use("/api/cars", carRoutes);
+        app.use("/api", userRoutes);
+        app.use("/api/ads", adsRoutes);
+        app.use("/api/user", profileRoutes);
+        app.use("/api/user", checkEmailProfile);
+        app.use("/api/user", checkUsernameProfile)
+        app.use("/api", reviewRoutes);
+
+        app.use((req, res) => {
+            res.status(404).json({error: "Route not found"});
+        });
+
+        app.use((err, req, res, next) => {
+            if (err instanceof SyntaxError && "body" in err) {
+                return res.status(400).json({error: "Invalid JSON format"});
+            }
+            next();
+        });
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.error("Error connecting to the database:", error);
+        process.exit(1)
     }
-    next();
-});
+}
 
-app.use(express.static(path.join(__dirname, "templates")));
-app.use(express.static(path.join(__dirname, "static")));
-app.use("/images", express.static(path.join(__dirname, "images")));
-
-
-const serveHTML = (req, res, filePath) => {
-    res.sendFile(path.join(__dirname, "templates", filePath));
-};
-
-app.get("/", (req, res) => serveHTML(req, res, "index.html"));
-app.get("/templates/authentication/signup.html", (req, res) => serveHTML(req, res, "authentication/signup.html"));
-app.get("/templates/authentication/login.html", (req, res) => serveHTML(req, res, "authentication/login.html"));
-app.get("/templates/cartrading/withlogout.html", (req, res) => serveHTML(req, res, "cartrading/withlogout.html"));
-app.get("/templates/cartrading/myAds.html", (req, res) => serveHTML(req, res, "cartrading/myAds.html"));
-app.get("/templates/cartrading/profile.html", (req, res) => serveHTML(req, res, "cartrading/profile.html"));
-app.get("/templates/cartrading/reviewExample.html", (req, res) => serveHTML(req, res, "cartrading/reviewExample.html"));
-app.get("/templates/index.html", (req, res) => serveHTML(req, res, "index.html"));
-
-
-app.use("/api/auth", signUpRoutes);
-app.use("/api/auth", loginRoutes);
-app.use("/api/check", checkUsername);
-app.use("/api/check", checkEmail);
-app.use("/api", searchRoutes);
-app.use("/api/cars", carRoutes);
-app.use("/api", userRoutes);
-app.use("/api/ads", adsRoutes);
-app.use("/api/user", profileRoutes);
-app.use("/api/user", checkEmailProfile);
-app.use("/api/user", checkUsernameProfile)
-app.use("/api", reviewRoutes);
-
-app.use((req, res) => {
-    res.status(404).json({error: "Route not found"});
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+startServer().catch((err) => {
+    console.error("Error starting server:", err);
+    process.exit(1);
+})
